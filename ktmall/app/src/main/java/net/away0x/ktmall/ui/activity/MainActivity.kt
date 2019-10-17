157976fc110732fa2943b427953bc82a650c4b22
Away0x <wutong0910@foxmail.com>
2019-10-17 13:41:58 +0800

    ktmall init

---
+++ ktmall/app/src/main/java/net/away0x/ktmall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package net.away0x.ktmall.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.away0x.ktmall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
